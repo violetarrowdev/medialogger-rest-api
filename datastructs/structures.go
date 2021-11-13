4d4c7c71d8297210c3896ef30911b2f043f7ec96
VioletArrow <violetarrowdev@gmail.com>
2021-11-13 14:59:14 -0500

    Finished designing basic structs

--- datastructs/structures.go
+++ datastructs/structures.go
@@ -6,7 +6,7 @@ type ListOrder struct {
 }
 
 type MediaItem struct {
-	UID            int    `json:"uid"` // has unique ID only within scope of user
+	UID            uint32 `json:"uid"` // has unique ID only within scope of user
 	Title          string `json:"title"`
 	ReleaseDate    string `json:"releaseDate"` // mm/dd/yyyy or mm/yyyy or yyyy
 	Medium         string `json:"medium"`
@@ -25,9 +25,11 @@ type MediaList struct {
 }
 
 type User struct {
-	UUID         int         `json:"uuid"` // unique across all users
-	Username     string      `json:"username"`
-	PasswordHash string      `json:"passwordHash"` // SHA-256
+	UUID         uint32      `json:"uuid,omitempty"` // unique across all users
+	Username     string      `json:"username"`       // must be unique
+	Email        string      `json:"email"`
+	PasswordHash string      `json:"passwordHash,omitempty"` // SHA-256
+	SessionToken uint8       `json:"-"`                      // TODO: temporary session token
 	SavedMedia   []MediaItem `json:"savedMedia"`
 	SavedLists   []MediaList `json:"savedLists"`
 }
---
+++ privacy/data.go
@@ -0,0 +1,33 @@
+package privacy
+
+import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"medialogger/server/datastructs"
+)
+
+func HashPassword(password string, uuid uint32) string {
+	input := password + string(uuid)
+	output := fmt.Sprintf("%x", sha256.Sum256([]byte(input)))
+	return output
+}
+
+// strips user PII and returns their data as JSON
+func StripPII(user *datastructs.User) string {
+	strippedUser := &datastructs.User{
+		UUID:         0,
+		Username:     user.Username,
+		Email:        user.Email,
+		PasswordHash: "",
+		SavedMedia:   user.SavedMedia,
+		SavedLists:   user.SavedLists,
+	}
+	userData, err := json.Marshal(strippedUser)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return ""
+	} else {
+		return string(userData)
+	}
+}
